63ca43b640de986f5dc971943c7164cb6301a72e
Kishin Manglani <kishin.manglani@gmail.com>
2022-03-29 16:58:46 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "DateToolsSwift",
     targets: [
         Target(name: "DateToolsSwift")
-    ]
+    ],
+    exclude: ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]    
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
+
